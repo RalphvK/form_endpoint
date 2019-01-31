eeae457122e525c19199337eef245b507ddd906e
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2019-01-31 22:08:14 +0100

    add env inspector script

---
+++ docs/scripts/env.php
@@ -0,0 +1,25 @@
+<?php
+
+    require __DIR__.'/config.php';
+
+    $back_end_path = COMPOSER_CFG::BACK_END_PATH;
+
+    // require composer packages
+    require $back_end_path.'vendor/autoload.php';
+
+    // load ENV
+    $dotenv = new Dotenv\Dotenv($back_end_path);
+    $dotenv->load();
+
+    foreach ($_ENV as $key => $value) {
+        if ($key == "CORS_WHITELIST") {
+            $origin_whitelist = explode(',', $_ENV['CORS_WHITELIST']);
+            foreach ($origin_whitelist as $index => $value) {
+                echo "\033[94m\"".$key."\"\033[0m => \033[92m\"".$value."\"\033[0m\n";
+            }
+        } else {
+            echo "\033[94m\"".$key."\"\033[0m => \033[92m\"".$value."\"\033[0m\n";
+        }
+    }
+
+?>
