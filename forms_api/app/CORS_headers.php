327389832fa5aae9d9f68c826980a61680634e88
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2019-01-31 22:29:09 +0100

    fix CORS whitelist not removing whitespace from ENV string

--- forms_api/app/CORS_headers.php
+++ forms_api/app/CORS_headers.php
@@ -1,13 +1,18 @@
 <?php
 
     // origin whitelist
-    $origin_whitelist = explode(',', $_ENV['CORS_WHITELIST']);
+    function get_origin_whitelist() {
+        $expression = '/,\s*/';
+        return preg_split($expression, $_ENV['CORS_WHITELIST']);
+    }
 
-    if (in_array($_SERVER["HTTP_ORIGIN"], $origin_whitelist)) {
-        header('Access-Control-Allow-Origin: '.$_SERVER["HTTP_ORIGIN"]);
-        header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-        header("Access-Control-Allow-Headers: X-Requested-With");
-        header("Access-Control-Allow-Credentials: true");
+    if (isset($_SERVER["HTTP_ORIGIN"])) {
+        if (in_array($_SERVER["HTTP_ORIGIN"], get_origin_whitelist())) {
+            header('Access-Control-Allow-Origin: '.$_SERVER["HTTP_ORIGIN"]);
+            header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+            header("Access-Control-Allow-Headers: X-Requested-With");
+            header("Access-Control-Allow-Credentials: true");
+        }
     }
 
 ?>
