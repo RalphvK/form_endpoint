2c7ff540182f80e3f58b8fe9e905e15776b80400
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2019-02-11 22:48:11 +0100

    add argIs function to composer debug script

--- docs/scripts/debug.php
+++ docs/scripts/debug.php
@@ -12,11 +12,16 @@
 
     if (isset($argv[1])) {
 
+        function arg_is($arg) {
+            global $argv;
+            return ($argv[1] == $arg);
+        }
+
         echo "\n\n\033[104m DEBUG START \033[0m\n";
         echo "\033[94m\n";
 
         // email test
-        if ($argv[1] == 'mail') {
+        if (arg_is('mail')) {
             $post = json_decode($_ENV['DEBUG_POST'], true);
             $formCfg = json_decode($_ENV['DEBUG_FORMCFG'], true);
             notify::callHooks($post, $formCfg);
